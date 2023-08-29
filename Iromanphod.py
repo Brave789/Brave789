c6fe4a14b740cb50e4158fb5ed8e4a5b0d342d4f
Brave789 <143253479+Brave789@users.noreply.github.com>
2023-08-29 20:01:27 +0530

    Create Iromanphod.py
    Hello ...... "World" 
    This  code is my another favourite one 
    And it is the second type or we can say that  this code is also very important

---
+++ Iromanphod.py
@@ -0,0 +1,15 @@
+x="he,world!"
+import random
+print(random.randrange(1,6))
+x=67
+y=34
+z=8
+i=7.88
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(i))
+print(x+y+z*i)
